f692705d4db668b57d1c61c3b408e15b05c2bf48
Vladimir Kuznetsov <mistakster@gmail.com>
2024-01-22 17:15:24 +0400

    added tests for the plugin

---
+++ test/plugin.spec.mjs
@@ -0,0 +1,48 @@
+import { it, expect } from 'vitest';
+import postcss from 'postcss';
+import plugin from "../lib/plugin.mjs";
+
+function process(css) {
+  return postcss([plugin])
+    .process(css, { from: undefined })
+    .then(result => result.css);
+}
+
+it('should work', async () => {
+  // language=CSS
+  const css = `
+      @media (min-width: 200px) and (min-width: 400px) {
+          .example {
+              padding: 0;
+          }
+      }
+  `;
+
+  await expect(process(css)).resolves.toMatchSnapshot();
+});
+
+it('should process (min-width: 0)', async () => {
+  // language=CSS
+  const css = `
+      @media (min-width: 200px) and (min-width: 0) {
+          .example {
+              padding: 0;
+          }
+      }
+  `;
+
+  await expect(process(css)).resolves.toMatchSnapshot();
+});
+
+it('should completely remove useless (min-width: 0)', async () => {
+  // language=CSS
+  const css = `
+      @media (min-width: 0) {
+          .example {
+              padding: 0;
+          }
+      }
+  `;
+
+  await expect(process(css)).resolves.toMatchSnapshot();
+});
